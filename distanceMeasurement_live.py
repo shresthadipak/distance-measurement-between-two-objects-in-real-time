cb11c7594f540cde7a9db07b56676f2040452163
Dipak <shresthadipak2049@gmail.com>
2023-01-19 20:19:17 +0545

    Feat: working in real-time video to measure the distance between person and dog

--- distanceMeasurement_live.py
+++ distanceMeasurement_live.py
@@ -1,23 +1,30 @@
 import cv2
 from objectDetectionModule import objectDetector
 
-cap = cv2.VideoCapture(0)
 
+detector = objectDetector()
+
+cap = cv2.VideoCapture('videos/test_video.mp4')
+width = int(cap.get(3))
+height = int(cap.get(4))
+fps = int(cap.get(5))/2
+
+out = cv2.VideoWriter('output/output3.avi', cv2.VideoWriter_fourcc(*'MPEG'), fps, (width, height))
 while True:
     ret, frame = cap.read()
 
     if not ret:
         break
 
-    detector = objectDetector()
-
     detect_img = detector.object_detect(frame)
 
-    cv2.imshow("Object Detection", detect_img)
+    out.write(detect_img)
+    # cv2.imshow("Object Detection", detect_img)
 
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
 
 cap.release()
+out.release()
 cv2.destroyAllWindows()
 
--- objectDetectionModule.py
+++ objectDetectionModule.py
@@ -113,78 +113,81 @@ class objectDetector():
                     dog_y_min = int(y0)
                     dog_y_max = int(y1)
 
+        try:
+            if width_d and width_p:
+                pixel_to_inches = int(np.mean(pixels_ratio_array)) 
 
-        pixel_to_inches = int(np.mean(pixels_ratio_array)) 
+                x_calc_1 = abs(person_x_min - dog_x_min)
+                x_calc_2 = abs(person_x_min - dog_x_max)
+                x_calc_3 = abs(person_x_max - dog_x_max)
+                x_calc_4 = abs(person_x_max - dog_x_min) 
 
-        x_calc_1 = abs(person_x_min - dog_x_min)
-        x_calc_2 = abs(person_x_min - dog_x_max)
-        x_calc_3 = abs(person_x_max - dog_x_max)
-        x_calc_4 = abs(person_x_max - dog_x_min) 
+                # print(str(x_calc_1))
+                # print(str(x_calc_2))
+                # print(str(x_calc_3))
+                # print(str(x_calc_4))
 
-        # print(str(x_calc_1))
-        # print(str(x_calc_2))
-        # print(str(x_calc_3))
-        # print(str(x_calc_4))
+                # print (math.sqrt((person_x_min - dog_x_max)**2 + (person_y_min - dog_y_max)**2))
 
-        # print (math.sqrt((person_x_min - dog_x_max)**2 + (person_y_min - dog_y_max)**2))
+                min_pixel_distance = min(x_calc_1, x_calc_2, x_calc_3, x_calc_4)
+                # print(min_pixel_distance)
+                # print(pixel_to_inches)
+                # print(pixels_ratio_array)
 
-        min_pixel_distance = min(x_calc_1, x_calc_2, x_calc_3, x_calc_4)
-        # print(min_pixel_distance)
-        # print(pixel_to_inches)
-        # print(pixels_ratio_array)
+                if x_calc_1 == min_pixel_distance:
+                        
+                        estimated_distance_inches = round(min_pixel_distance / pixel_to_inches, 2)
+                        distance_start_point = (dog_x_min, dog_y_max - height_d)
+                        distance_end_point = (person_x_min, person_y_max - height_p)
+                        distance_text_point = (dog_x_min + int(x_calc_1 / 5), (person_y_max - height_p) - 10)
+                        
+                        print("x_calc_1 - smallest: " + str(x_calc_1))
+                        print("Estimated Inches: " + str(estimated_distance_inches))
 
-        if x_calc_1 == min_pixel_distance:
-                
-                estimated_distance_inches = round(min_pixel_distance / pixel_to_inches, 2)
-                distance_start_point = (dog_x_min, dog_y_max - height_d)
-                distance_end_point = (person_x_min, person_y_max - height_p)
-                distance_text_point = (dog_x_min + int(x_calc_1 / 5), (person_y_max - height_p) - 10)
-                
-                print("x_calc_1 - smallest: " + str(x_calc_1))
-                print("Estimated Inches: " + str(estimated_distance_inches))
+                elif x_calc_2 == min_pixel_distance:
+                        
+                        estimated_distance_inches = round(min_pixel_distance / pixel_to_inches, 2)
+                        distance_start_point = (dog_x_max, dog_y_max - height_d)
+                        distance_end_point = (person_x_min, person_y_max - height_p)
+                        distance_text_point = (dog_x_max + int(x_calc_2 / 5), (person_y_max - height_p) - 20)
+                        
+                        print("x_calc_2 - smallest: " + str(x_calc_2))
+                        print("Estimated Inches: " + str(estimated_distance_inches))  
 
-        elif x_calc_2 == min_pixel_distance:
-                
-                estimated_distance_inches = round(min_pixel_distance / pixel_to_inches, 2)
-                distance_start_point = (dog_x_max, dog_y_max - height_d)
-                distance_end_point = (person_x_min, person_y_max - height_p)
-                distance_text_point = (dog_x_max + int(x_calc_2 / 5), (person_y_max - height_p) - 20)
-                
-                print("x_calc_2 - smallest: " + str(x_calc_2))
-                print("Estimated Inches: " + str(estimated_distance_inches))  
+                elif x_calc_3 == min_pixel_distance:
+                        
+                        estimated_distance_inches = round(min_pixel_distance / pixel_to_inches, 2)
+                        distance_start_point = (person_x_max, person_y_max - height_p)
+                        distance_end_point = (dog_x_max, dog_y_max - height_d)
+                        distance_text_point = (person_x_max + int(x_calc_3 / 5), (person_y_max - height_p) - 20)
+                        
+                        print("x_calc_3 - smallest: " + str(x_calc_3))
+                        print("Estimated Inches: " + str(estimated_distance_inches)) 
 
-        elif x_calc_3 == min_pixel_distance:
-                
-                estimated_distance_inches = round(min_pixel_distance / pixel_to_inches, 2)
-                distance_start_point = (person_x_max, person_y_max - height_p)
-                distance_end_point = (dog_x_max, dog_y_max - height_d)
-                distance_text_point = (person_x_max + int(x_calc_3 / 5), (person_y_max - height_p) - 20)
-                
-                print("x_calc_3 - smallest: " + str(x_calc_3))
-                print("Estimated Inches: " + str(estimated_distance_inches)) 
+                else:
+                        
+                        estimated_distance_inches = round(min_pixel_distance / pixel_to_inches, 2)
+                        distance_start_point = (person_x_max, person_y_max - height_p)
+                        distance_end_point = (dog_x_min, dog_y_max - height_d)
+                        distance_text_point = (person_x_max + int(x_calc_4 / 5), (person_y_max - height_p) - 20)
+                        
+                        print("x_calc_4 - smallest: " + str(x_calc_4))
+                        print("Estimated Inches: " + str(estimated_distance_inches))       
 
-        else:
-                
-                estimated_distance_inches = round(min_pixel_distance / pixel_to_inches, 2)
-                distance_start_point = (person_x_max, person_y_max - height_p)
-                distance_end_point = (dog_x_min, dog_y_max - height_d)
-                distance_text_point = (person_x_max + int(x_calc_4 / 5), (person_y_max - height_p) - 20)
-                
-                print("x_calc_4 - smallest: " + str(x_calc_4))
-                print("Estimated Inches: " + str(estimated_distance_inches))       
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                font_color = (255, 255, 255)
+                font_thickness = 2 
+                font_scale = 1
 
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        font_color = (255, 255, 255)
-        font_thickness = 2 
-        font_scale = 1
+                distance_color = (255, 255, 255)
+                distance_thickness = 2
+                if draw:
+                    cv2.circle(img, distance_start_point, 5, (0, 0, 255), -1)
+                    cv2.circle(img, distance_end_point, 5, (0, 0, 255), -1)
+                    cv2.line(img, distance_start_point, distance_end_point, distance_color, distance_thickness)
 
-        distance_color = (255, 255, 255)
-        distance_thickness = 2
-        if draw:
-            cv2.circle(img, distance_start_point, 5, (0, 0, 255), -1)
-            cv2.circle(img, distance_end_point, 5, (0, 0, 255), -1)
-            cv2.line(img, distance_start_point, distance_end_point, distance_color, distance_thickness)
+                    cv2.putText(img, "~" + str(estimated_distance_inches) + " in.", distance_text_point, font, font_scale, font_color, font_thickness, cv2.LINE_AA)    
 
-            cv2.putText(img, "~" + str(estimated_distance_inches) + " in.", distance_text_point, font, font_scale, font_color, font_thickness, cv2.LINE_AA)    
-
-        return img
+                return img
+        except:
+            pass
